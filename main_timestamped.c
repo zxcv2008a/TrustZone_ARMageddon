46f83a7dab3126f3e7d0475dc8e7f65369febb5a
Mostafa Bestawy <34907222+zxcv2008a@users.noreply.github.com>
2020-09-20 20:40:39 -0400

    Update main_timestamped.c

--- main_timestamped.c
+++ main_timestamped.c
@@ -311,18 +311,18 @@ void main(void)
 
 	add(arr,top);
 	tfm_ipc_test_1001();
-  printk("Test 1001 = %f\n", test1001/ CLOCKS_PER_SEC);
+  printk("Test 1001 = %f\n", test1001);
 
 	tfm_ipc_test_1002();
-  printk("Test 1002 = %f\n", test1002/ CLOCKS_PER_SEC);
+  printk("Test 1002 = %f\n", test1002);
 	tfm_ipc_test_1003();
-  printk("Test 1003 = %f\n", test1003/ CLOCKS_PER_SEC);
+  printk("Test 1003 = %f\n", test1003);
 	tfm_ipc_test_1004();
-  printk("Test 1004 = %f\n", test1004/ CLOCKS_PER_SEC);
+  printk("Test 1004 = %f\n", test1004);
 	tfm_ipc_test_1005();
-  printk("Test 1005 = %f\n", test1005/ CLOCKS_PER_SEC);
+  printk("Test 1005 = %f\n", test1005);
 	tfm_ipc_test_1006();
-  printk("Test 1006 = %f\n", test1006/ CLOCKS_PER_SEC);
+  printk("Test 1006 = %f\n", test1006);
 
 	printk("TF-M IPC on %s\n", CONFIG_BOARD);
 }
